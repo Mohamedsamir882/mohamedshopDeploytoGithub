1e382ac8ce867e494ec614277ea3ae806908ac04
mohamed samir <mohamed7745727@gmail.com>
2020-06-05 06:37:08 +0300

    all source code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductComponent } from './product/product.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminProductComponent } from './admin/admin-product/admin-product.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { ShoppingCardComponent } from './shopping-card/shopping-card.component';
+import { LoginComponent } from './login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'product', component: ProductComponent},
+  {path: 'orders', component: MyOrdersComponent},
+  {path: 'admin/product', component: AdminProductComponent},
+  {path: 'Admin/orders', component: AdminOrdersComponent},
+  {path: 'shopping-card', component: ShoppingCardComponent},
+  {path: 'login', component: LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule }from './app-routing.module';
+import { AppComponent }from './app.component';
+import { NavBarComponent }from './nav-bar/nav-bar.component';
+import { ProductComponent }from './product/product.component';
+import { MyOrdersComponent }from './my-orders/my-orders.component';
+import {AdminProductComponent}from  './admin/admin-product/admin-product.component';
+import {AdminOrdersComponent}from './admin/admin-orders/admin-orders.component';
+import { NgbModule }from '@ng-bootstrap/ng-bootstrap';
+import { ShoppingCardComponent }from './shopping-card/shopping-card.component';
+import { LoginComponent }from './login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ProductComponent,
+    MyOrdersComponent,
+    AdminOrdersComponent,
+    AdminProductComponent,
+    ShoppingCardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
